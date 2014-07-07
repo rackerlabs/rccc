d4031e15db788e4260e33143b5d0d394f06f8a79
Ash Wilson <smashwilson@gmail.com>
2014-07-07 11:39:50 -0400

    Bootstrapping script fixes.

--- fog/bootstrap.sh
+++ fog/bootstrap.sh
@@ -4,8 +4,9 @@ mkdir fog
 cd fog
 
 # Get a modern Ruby
-sudo add-apt-repository ppa:/brightbox-rubyng
-sudo apt-get ruby2.1
+sudo add-apt-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install -y ruby2.1 ruby2.1-dev
 
 # Install fog
 sudo gem install fog
